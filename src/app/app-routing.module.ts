4afd0415a30550c800c30068f0d2228d48f0d2cb
fumihiro.sunada <fumihiro.sunada@bigtreetc.com>
2019-05-04 09:46:47 +0900

    Minor bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  // { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  // { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
 ];
 
 @NgModule({
